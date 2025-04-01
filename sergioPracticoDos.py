51075ca8cf88c19996d89c0d3e668d15fe7aaee3
ElBenjas4000 <sbmartingonzalez@escuelasproa.edu.ar>
2025-04-01 20:18:07 -0300

    Update sergioPracticoDos.py

--- sergioPracticoDos.py
+++ sergioPracticoDos.py
@@ -1,2 +1,14 @@
-import nltk
+import nltk, os
 
+def clear():
+    os.system('cls')
+
+clear()
+print("    ### Contador de pronombres en Python ###")
+
+pronombres = ["yo", "tu", "el", "ella", "usted", "nosotros", "nosotras", "vosotros", "ellos", "ellas", "ustedes", "me", "te", "lo", "la", "nos", "os", "los", "las", "le", "les", "se"]
+texto = str(input("Ingrese su texto >> "))
+palabras = nltk.word_tokenize(texto.lower())
+
+pronombres_encontrados = [pronombre for pronombre in pronombres if pronombre in pronombres]
+print(palabras)
