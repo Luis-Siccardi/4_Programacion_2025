203c9a25e60f7c843abd42ce7acb82e8c8a983e8
Andres <agmorillofigueroa@escuelasproa.edu.ar>
2025-04-02 14:35:27 -0300

    Add files via upload

---
+++ AndresPracticoUno.py
@@ -0,0 +1,51 @@
+'''
+Trabajo práctico 1 y 2
+Luis Alberto Siccardi
+
+
+Luego del repaso realizado la semana pasada, tenés que resolver los dos ejercicios planteados a continuación, ambos deben entregarse por GitHub y ser defendidos oralmente. 
+Cada trabajo debe ser guardado en un archivo que se llame de la siguiente forma "nombrePracticoUno.py" y "nombrePracticoDos.py" usando la 
+
+notación de camello.
+Fecha de entrega: 02/04/25
+
+Ejercicio 1: Lista de números pares.
+Escribe un programa que solicite al usuario ingresar 10 números y luego imprima una lista con los números pares ingresados.
+
+Ejercicio 2: Contar pronombres en el texto compartido por la Profesora Lorena.
+Escribe un programa que identifique y cuente los pronombres de un texto y luego imprima la cantidad que contiene el texto.
+'''
+
+def contarPares(lista):
+    resultado=[]
+    for i in range ( len(lista) ):
+        if lista[i]%2==0:
+            print(lista[i])
+            resultado.append(lista[i])
+    return resultado
+
+lista=[]
+for i in range(10): # <-- esoeso
+    numero=int(input(f'{i+1}:'))
+    lista.append(numero)
+print(lista)
+
+print(contarPares(lista))
+
+texto='buenas tardes yo bien.'
+
+hola=texto.split(' ')
+
+pronombres=['yo','tu','el','esta']
+
+print(hola)
+
+textoFinal=''
+
+for i in hola:
+    if i in pronombres:
+        textoFinal = f'{textoFinal} >>{i}<<'
+    else:
+        textoFinal = f'{textoFinal} {i}'
+
+print(textoFinal)
