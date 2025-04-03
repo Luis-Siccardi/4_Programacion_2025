29908aa2d1da1a3196e17d8ecfbecb3935bb9c77
ConstanzaMayr <cmayr@escuelasproa.edu.ar>
2025-04-03 11:42:40 -0300

    Coty
    mis trabajos practicos de programación

---
+++ constanzamayrPracticoDos.py
@@ -0,0 +1,35 @@
+print ("Ejercicio 2: Contar pronombres")
+print ("                                    ") #para que quede mas prolijo
+
+#Ejercicio 2: Contar pronombres en el texto compartido por la Profesora Lorena.
+
+#Escribe un programa que identifique y cuente los pronombres de un texto y luego imprima la cantidad que contiene el texto.
+
+print ("Vamos a identificar los ponombres en en siguiente texto!!")
+print ("                                    ")
+
+texto='''Stéfano habita una vieja casa de barrio pobre. Él vive allí desde hace mucho tiempo. El humilde hogar es de tres piezas; dos, dan a la calle; la otra es de madera y cinc y recuadra con un patio lleno de viento. La casa tiene varias ventanas. La que está a la derecha lleva al zaguán oscuro; la de la izquierda a la otra sala. La humedad es tan antigua que ya no queda ese olor ácido y penetrante, sin embargo las huellas verdinegras perduran en las paredes y en los techos, los cuales, crujen al compás del viento. Stéfano no tuvo en su vida lo que deseaba, sin embargo, la carencia le ayudó a que él nunca se rindiera ante su destino. Lo único que le interesaba era lograr lo que soñaba.'''
+
+print (texto)
+print ("                                    ")
+
+pronombres= ['yo', 'tú', 'él', 'ella', 'usted', 'nosotros', 'nosotras', 'vosotros', 'vosotras', 'ellos', 'ellas', 'ustedes', 'me', 'te', 'lo', 'la', 'nos', 'os', 'los', 'las', 'le', 'se', 'mi', 'mía', 'mío', 'mía', 'nuestro', 'nuestra', 'nuestros', 'nuestras', 'tuyo', 'tuya', 'tuyos', 'tuyas', 'suyo', 'suya', 'suyos', 'suyas', 'este', 'esta', 'estos', 'estas', 'ese', 'esa', 'esos', 'esas', 'aquel', 'aquella', 'aquellos', 'aquellas', 'algo', 'alguien', 'alguno', 'alguna', 'algunos', 'algunas', 'nadie', 'ninguno', 'ninguna', 'todos', 'todas', 'mucho', 'mucha', 'muchos', 'muchas', 'poco', 'poca', 'pocos', 'pocas', 'varios', 'varias', 'cualquiera', 'bastante', 'demasiado', 'menos', 'cada', 'ningún', 'ninguna', 'que', 'cual', 'quienes', 'cuyo', 'cuya', 'cuyos', 'cuyas', 'lo que', 'lo cual', 'qué', 'cuál', 'cuáles', 'quién', 'quiénes', 'cuánto', 'cuánta', 'cuántos', 'cuántas', 'cómo', 'dónde', 'cuándo', 'por qué', 'se']
+
+    # Dividir el texto en palabras
+
+palabras = texto.lower().split()
+
+    #paréntesis indican que estás llamando a un método o función
+    #lower=todo a minuscula 
+    #split=separa las palabras
+
+    # Contador de pronombres encontrados
+
+contador_pronombres = 0
+
+    # Recorrer el texto y contar los pronombres
+for i in palabras:
+    if i in pronombres:
+        contador_pronombres += 1
+
+print(f"El texto contiene {contador_pronombres} pronombres.")
---
+++ constanzamayrPracticoUno.py
@@ -0,0 +1,34 @@
+
+print ("Ejercicio 1: Lista de números pares.")
+print ("") #oara que quede mas prolijo
+
+#Ejercicio 1: Lista de números pares.
+#Escribe un programa que solicite al usuario ingresar 10 números y luego imprima una lista con los números pares ingresados.
+
+print ("Hola!! Este es mi programa, vos podras ingresar solo 10 numeros y solo te devolvera los pares, pongamos lo a prueba!!")
+print ("") 
+
+numeros=[]
+
+#range determina la cantidad de veces que se repita el bluce, q esta determinado por "cantidad_num"
+
+for i in range(10):
+    num = int(input(f"Ingrese el número {i+1}: "))  # Solicita el número y lo agrega a la lista
+    numeros.append(num) #ingresa en la lista vacia "numeros" el valor de num
+
+#append es para agregar valores a listas
+
+print(f "la lista de todos los números hasta ahora es: {numeros}") 
+
+#chequeamos que los numeros se hayan guardado correctamente en la lista
+
+
+pares=[] #lista de números vacia de pares, que ahora en el bucle determinaremos cuales son los números pares que iran aca
+
+for num in numeros: #es un bucle que recorre la lista numeros
+
+#num hace referencia que en cada iteración, toma el siguiente número de la lista y lo asigna a la variable num
+
+    if num % 2 ==0: #si num dividido dos es igual a 0 se agrega a la lista pares 
+      pares.append(num)
+print(f "los numeros pares son: {pares}")
