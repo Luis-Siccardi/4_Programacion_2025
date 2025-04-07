d8714e81430c184c97f1c06acfdd332e1b590854
MateoOrtega10 <mcortega@escuelasproa.edu.ar>
2025-04-07 12:04:25 -0300

    Update mateoOrtegaPracticoUno.py

--- mateoOrtegaPracticoUno.py
+++ mateoOrtegaPracticoUno.py
@@ -1,8 +1,8 @@
-Listanumero=[]
-ListaPares=[]
-for i in range(10):
+Listanumero=[]#lista vacia
+ListaPares=[]#lista vacia
+for i in range(10):#hace un for a un rango de 10 
     numUsuario= int(input("ingrese su numeros: "))
-    Listanumero.append(numUsuario)
-    if numUsuario %2 == 0:
-        ListaPares.append(numUsuario)
-print(ListaPares)
+    Listanumero.append(numUsuario)#lleva los numeros ingresados a listanumero
+    if numUsuario %2 == 0:#hace el calculo para que de pares
+        ListaPares.append(numUsuario)#a esos pares los manda a la listapares
+print(ListaPares)#imprime listapares
